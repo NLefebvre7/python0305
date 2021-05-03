95b2f7146788b5eb306d3216184b4ef0759b34c4
NLefebvre7 <57395815+NLefebvre7@users.noreply.github.com>
2021-05-03 17:28:13 +0200

    Update pendu.py

--- jour1/pendu/pendu.py
+++ jour1/pendu/pendu.py
@@ -11,14 +11,14 @@ mot_devine = "-" * len(mot)
 print(mot_devine)     
 
 for i in range(5):
-while mot_devine!=mot:                                                                   
-lettre=input("Entrez une lettre : ")                                  
-for i in range(len(mot)):                                                        
-if lettre==mot[i]:                                                                     
-mot_devine = mot_devine[:i] + lettre + mot_devine[i+1:]                 
+    while mot_devine!=mot:                                                                   
+        lettre=input("Entrez une lettre : ")                                  
+        for i in range(len(mot)):                                                        
+            if lettre==mot[i]:                                                                     
+        mot_devine = mot_devine[:i] + lettre + mot_devine[i+1:]                 
                                                                         
                                                                        
-print(mot_devine)
-if mot == mot_devine: 
-print ('Bravo ! Le mot',mot,'a été trouvé')
-break
+        print(mot_devine)
+        if mot == mot_devine: 
+        print ('Bravo ! Le mot',mot,'a été trouvé')
+        break
